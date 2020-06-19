1d8fd1aeaf3b1255b16258cd1af96baaafbb8995
zzlaodaizz7 <kaka.33.yb01@gmail.com>
2020-06-19 22:14:05 +0700

    ###

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
+Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
